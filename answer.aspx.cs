31d49da76bc5487822f335ba8f9665f53e6d9ea1
Hatem Mira <hatemmira90@gmail.com>
2012-06-28 18:36:25 +0200

    LiteralProfile Handled in backend
    +mini micro and full profiles implemented in front end and back end
    +Literal profile for literal answers is retrieved in back end

--- answer.aspx.cs
+++ answer.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using weetit_website.ServiceReference1;
+using weetit_website.classes;
 
 namespace weetit_website
 {
@@ -12,15 +14,138 @@ namespace weetit_website
         public answer()
         {
 
-          
+
 
         }
 
         protected void Page_Load(object sender, EventArgs e)
         {
-
-         
-
+            String question = Request.QueryString["q"];
+            String HTML = "";
+            if (question != null)
+            {
+                List<List<Profile>> diffProfiles = QuestionAnswerer.getAnswersInProfiles(question);
+                foreach (List<Profile> profiles in diffProfiles)
+                {
+                    foreach (Profile profile in profiles)
+                    {
+                        if (profile is FullProfile)
+                        {
+                            FullProfile fullProfile = (FullProfile)profile;
+                            String relatedImg = "";
+                            String details = "";
+                            foreach (KeyValuePair<String, Entity[]> key in fullProfile.Details.ToList())
+                            {
+                                if (key.Value.Length != 0)
+                                {
+                                    details += "<tr>"
+                                            + "<td class=\"property\">"
+                                                + key.Key
+                                            + "</td>"
+                                            + "<td class=\"value\">";
+                                    foreach (Entity en in key.Value)
+                                        details += "<div class=\"line\">"
+                                                    + en.Label
+                                                + "</div>";
+                                    details += "</td>"
+                                            + "</tr>";
+                                }
+                            }
+                            foreach (Entity rel in fullProfile.Related.ToList())
+                                relatedImg += "<img src=\"" + rel.Picture + "\"/>";
+                            HTML += "<div class=\"fullprofile\">"
+                                    + "<div class=\"abstractcontainer\">"
+                                        + "<div class=\"profilepic\">"
+                                            + "<img src=\"" + fullProfile.Picture + "\" />"
+                                        + "</div>"
+                                        + "<div class=\"abstract\">"
+                                            + "<a href=\"#\" class=\"title\">" + fullProfile.Label + "</a>"
+                                            + "<p class=\"abstracttext\">"
+                                                + fullProfile.Abstract
+                                            + "</p>"
+                                        + "</div>"
+                                        + "<div class=\"clearfix\">"
+                                        + "</div>"
+                                    + "</div>"
+                                    + "<div class=\"relatedresults\">"
+                                        + "<div class=\"subtitle\">"
+                                            + "Related results</div>"
+                                        + relatedImg
+                                    + "</div>"
+                                    + "<div class=\"profiledetails\">"
+                                        + "<div class=\"subtitle\">"
+                                            + "Profile details</div>"
+                                        + "<table class=\"detailstable\">"
+                                            + details
+                                        + "</table>"
+                                    + "</div>"
+                                + "</div>";
+                        }
+                        else if (profile is MiniProfile)
+                        {
+                            MiniProfile miniProfile = (MiniProfile)profile;
+                            String details = "";
+                            foreach (KeyValuePair<String, Entity[]> key in miniProfile.Details.ToList())
+                            {
+                                if (key.Value.Length != 0)
+                                {
+                                    details += "<tr>"
+                                            + "<td class=\"property\">"
+                                                + key.Key
+                                            + "</td>"
+                                            + "<td class=\"value\">";
+                                    foreach (Entity en in key.Value)
+                                        details += "<div class=\"line\">"
+                                                    + en.Label
+                                                + "</div>";
+                                    details += "</td>"
+                                            + "</tr>";
+                                }
+                            }
+                            HTML += "<div class=\"miniprofile\">"
+                                        + "<div class=\"minileft\">"
+                                            + "<img src=\"" + miniProfile.Picture + "\" />"
+                                        + "</div>"
+                                        + "<div class=\"miniright\">"
+                                            + "<div>"
+                                                + "<a class=\"title\" href=\"#\">" + miniProfile.Label + "</a>"
+                                            + "</div>"
+                                        + "<div class=\"abstract\">"
+                                            + miniProfile.Abstract
+                                        + "</div>"
+                                        + "<div class=\"minitable\">"
+                                            + "<table class=\"detailstable\">"
+                                                + details
+                                            + "</table>"
+                                        + "</div>"
+                                    + "</div>"
+                                    + "<div class=\"clearfix\">"
+                                    + "</div>"
+                                + "</div>";
+                        }
+                        else if (profile is MicroProfile)
+                        {
+                            MicroProfile microProfile = (MicroProfile)profile;
+                            HTML += "<div class=\"microprofile\">"
+                                + "<div class=\"left\">"
+                                    + "<img src=\"" + microProfile.Picture + "\" />"
+                                + "</div>"
+                                + "<div class=\"right\">"
+                                    + "<div>"
+                                        + "<a class=\"title\" href=\"#\">" + microProfile.Label + "</a>"
+                                    + "</div>"
+                                    + "<div class=\"abstract\">"
+                                        + microProfile.Abstract
+                                    + "</div>"
+                                + "</div>"
+                                + "<div class=\"clearfix\">"
+                                + "</div>"
+                            + "</div>";
+                        }
+                    }
+                }
+            }
+            answerbox.InnerHtml = HTML;
         }
     }
 }
--- classes/QuestionAnswerer.cs
+++ classes/QuestionAnswerer.cs
@@ -2,50 +2,92 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
 using weetit_website.ServiceReference1;
 
-namespace weetit_website
+namespace weetit_website.classes
 {
     public class QuestionAnswerer
     {
         public static List<List<Profile>> getAnswersInProfiles(String question)
         {
             QAServiceInterfaceClient QAClient = new QAServiceInterfaceClient();
-            ProfileConstructorInterfaceClient profileClient= new ProfileConstructorInterfaceClient();
+            ProfileConstructorInterfaceClient profileClient = new ProfileConstructorInterfaceClient();
             List<questionAnswer> answersForDiffQuestions = QAClient.GetAnswerWithQuestionStructure(question).ToList();
             List<List<Profile>> allAnswers = new List<List<Profile>>();
+            bool isKeywordSearch = true;
             foreach (questionAnswer qAnswer in answersForDiffQuestions)
+                if (qAnswer.predicateUriList.Count != 0 || qAnswer.subjectUriList.Count != 0)
+                {
+                    isKeywordSearch = false;
+                    break;
+                }
+            if (isKeywordSearch == false)
             {
-                if (qAnswer.questiontype == utilquestionTypes.URIAsnwer)
+                foreach (questionAnswer qAnswer in answersForDiffQuestions)
                 {
-                    List<String> answers=qAnswer.objectUriList.ToList();
-                    if (answers.Count == 1)
+                    if (qAnswer.questiontype == utilquestionTypes.URIAsnwer)
                     {
-                        List<Profile> fullProfile = new List<Profile>();
-                        fullProfile.Add(profileClient.ConstructProfile(answers[0], MergedServicechoiceProfile.full, 15));
-                        allAnswers.Add(fullProfile);
+                        List<String> answers = qAnswer.objectUriList.ToList();
+                        if (answers.Count == 1)
+                        {
+                            List<Profile> fullProfile = new List<Profile>();
+                            fullProfile.Add(profileClient.ConstructProfile(answers[0], MergedServicechoiceProfile.full, 15));
+                            allAnswers.Add(fullProfile);
+                        }
+                        else if (answers.Count < 4 && answers.Count>1)
+                        {
+                            List<Profile> miniProfiles = new List<Profile>();
+                            foreach (String answer in answers)
+                                miniProfiles.Add(profileClient.ConstructProfile(answer, MergedServicechoiceProfile.mini, 7));
+                            allAnswers.Add(miniProfiles);
+                        }
+                        else
+                        {
+                            List<Profile> microProfile = new List<Profile>();
+                            foreach (String answer in answers)
+                                microProfile.Add(profileClient.ConstructProfile(answer, MergedServicechoiceProfile.micro, 7));
+                            allAnswers.Add(microProfile);
+                        }
                     }
-                    else if (answers.Count < 4)
+                    else if (qAnswer.questiontype == utilquestionTypes.literalAnswer)
                     {
-                        List<Profile> miniProfiles = new List<Profile>();
-                        foreach (String answer in answers)
-                            miniProfiles.Add(profileClient.ConstructProfile(answer, MergedServicechoiceProfile.mini, 7));
-                        allAnswers.Add(miniProfiles);
+                        List<Profile> LPLIST = new List<Profile>();
+                        // string h= qAnswer.subjectUriList.Keys.ToList()[0];
+                        LPLIST.Add(profileClient.ConstructLiteralProfile(qAnswer.subjectUriList.Keys.ToList()[0], qAnswer.predicateLabelList.Keys.ToList()[0], qAnswer.subjectLabelList.Keys.ToList()[0], "jkfjk", qAnswer.objectUriList[0], qAnswer.predicateUriList.Keys.ToList()[0]));
+
+                        allAnswers.Add(LPLIST);
+
+
                     }
-                    else
-                    {
-                        List<Profile> microProfile = new List<Profile>();
-                        foreach (String answer in answers)
-                            microProfile.Add(profileClient.ConstructProfile(answer, MergedServicechoiceProfile.micro, 7));
-                        allAnswers.Add(microProfile);
-                    }
+
                 }
-                else if (qAnswer.questiontype == utilquestionTypes.literalAnswer)
+            }
+            else
+            {
+                List<String> answers = new List<String>();
+                foreach (questionAnswer answer in answersForDiffQuestions)
+                    if (answer.objectUriList.Length > 0)
+                        answers.Add(answer.objectUriList[0]);
+                if (answers.Count == 1)
                 {
-                    
+                    List<Profile> fullProfile = new List<Profile>();
+                    fullProfile.Add(profileClient.ConstructProfile(answers[0], MergedServicechoiceProfile.full, 15));
+                    allAnswers.Add(fullProfile);
                 }
-                //literal answer starts here 
+                else if (answers.Count < 4 && answers.Count>1)
+                {
+                    List<Profile> miniProfiles = new List<Profile>();
+                    foreach (String answer in answers)
+                        miniProfiles.Add(profileClient.ConstructProfile(answer, MergedServicechoiceProfile.mini, 7));
+                    allAnswers.Add(miniProfiles);
+                }
+                else
+                {
+                    List<Profile> microProfile = new List<Profile>();
+                    foreach (String answer in answers)
+                        microProfile.Add(profileClient.ConstructProfile(answer, MergedServicechoiceProfile.micro, 7));
+                    allAnswers.Add(microProfile);
+                }
             }
             return allAnswers;
         }
