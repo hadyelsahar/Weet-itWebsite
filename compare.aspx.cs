cb81f06e1f9bb3cc83cbca20914c7f7762c6ca4b
sherif kandeel <sherifkandel4@gmail.com>
2012-07-04 00:21:14 +0200

    compare now works with maps and labels and spans
    compare now works with maps and labels and spans
    also with try{} and catch{}

--- compare.aspx.cs
+++ compare.aspx.cs
@@ -12,158 +12,183 @@ namespace weetit_website
         protected void Page_Load(object sender, EventArgs e)
         {
             //if there's no query, return the original page (this case should never happen)
-            if (Request.QueryString["q"] == null)
-                return;
-
-            //parsing the parameters
-            List<string> parsedStringsList = parseString(Request.QueryString["q"].ToString());
-
-            //the outputURIs
-            List<string> outputURIs = new List<string>();
-            
-            //using the keyword search to get the URIs
-            keywordSearchServiceInterfaceClient kwClient = new keywordSearchServiceInterfaceClient();
-            foreach (string item in parsedStringsList)
+            try
             {
-                outputURIs.Add(kwClient.geturi_bestMatch(item));
-
-            }
-
-
-            //comparing between the translated uris
-            List<ResourceInformation> comparisonOutput = new List<ResourceInformation>();
-            ComparisonServiceInterfaceClient comparisonClient = new ComparisonServiceInterfaceClient();
-            comparisonOutput = comparisonClient.Compare(outputURIs.ToArray()).ToList();
+                if (Request.QueryString["q"] == null)
+                {
+                    answerbox.InnerHtml = "<h3> No parameters to compare between... </h3>";
+                    return;
+                }
+                else
+                {
 
 
-            // comparisonOutput = preprocessingComparisonObjects(comparisonOutput);
+                    //parsing the parameters
+                    List<string> parsedStringsList = parseString(Request.QueryString["q"].ToString());
 
-            //getting the micro profiles
-            ProfileConstructorInterfaceClient constructProfile = new ProfileConstructorInterfaceClient();
-            List<MicroProfile> MicroProfilesList = new List<MicroProfile>();
+                    //the outputURIs
+                    List<string> outputURIs = new List<string>();
 
-            foreach (string item in outputURIs)
-            {
-                MicroProfilesList.Add((MicroProfile)constructProfile.ConstructProfile(item, MergedServicechoiceProfile.micro, 10));
+                    //using the keyword search to get the URIs
+                    keywordSearchServiceInterfaceClient kwClient = new keywordSearchServiceInterfaceClient();
+                    foreach (string item in parsedStringsList)
+                    {
+                        outputURIs.Add(kwClient.geturi_bestMatch(item));
 
-            }
-
-
-            //output to the divs
-            //First the microProfile
-            answerbox.InnerHtml = "";
-            answerbox.InnerHtml +=
-                "<div class=\"comparisonbox tableheader\">" +
-                "<div class=\"predicate\">" +
-                    "<p></p>" +
-                "</div>" +
-                "<div class=\"window\">" +
-                    "<div class=\"datarow\">";
-
-            //this was added to handle the maps if available
+                    }
 
 
-            foreach (MicroProfile item in MicroProfilesList)
-            {
-                answerbox.InnerHtml += "<div class=\"datacell\">" +
-                            "<img src=\"" + item.Picture + "\" />" +
-                            "<div class=\"title\">" + item.Label + "</div>" +
-                    //"<div class=\"Abstract\">" + item.Abstract + "</div>" +
+                    //comparing between the translated uris
+                    List<ResourceInformation> comparisonOutput = new List<ResourceInformation>();
+                    ComparisonServiceInterfaceClient comparisonClient = new ComparisonServiceInterfaceClient();
+                    comparisonOutput = comparisonClient.Compare(outputURIs.ToArray()).ToList();
+
+
+                    // comparisonOutput = preprocessingComparisonObjects(comparisonOutput);
+
+                    //getting the micro profiles
+                    ProfileConstructorInterfaceClient constructProfile = new ProfileConstructorInterfaceClient();
+                    List<MicroProfile> MicroProfilesList = new List<MicroProfile>();
+
+                    foreach (string item in outputURIs)
+                    {
+                        MicroProfilesList.Add((MicroProfile)constructProfile.ConstructProfile(item, MergedServicechoiceProfile.micro, 10));
+
+                    }
+
+
+                    //output to the divs
+                    //First the microProfile
+                    answerbox.InnerHtml = "";
+                    answerbox.InnerHtml +=
+                        "<div class=\"comparisonbox tableheader\">" +
+                        "<div class=\"predicate\">" +
+                            "<p></p>" +
+                        "</div>" +
+                        "<div class=\"window\">" +
+                            "<div class=\"datarow\">";
+
+                    //this was added to handle the maps if available
+
+
+                    foreach (MicroProfile item in MicroProfilesList)
+                    {
+                        answerbox.InnerHtml += "<div class=\"datacell\">" +
+                                    "<a href=\"answer.aspx?uri=" + item.URI + " \"><img src=\"" + item.Picture + "\" />  </a>" +
+                                    "<div class=\"title\"> <a href=\"answer.aspx?uri=" + item.URI + "\">" + item.Label + "</a></div>" +
+                            //"<div class=\"Abstract\">" + item.Abstract + "</div>" +
+                                "</div>";
+
+                    }
+
+                    answerbox.InnerHtml += "</div>" +
+                        "</div>" +
+                        "<div class=\"clearfix\">" +
+                        "</div>" +
+                    "</div>";
+
+                    //right till now
+                    //will crash if no comparison........
+
+
+                    //now for the data for each entity
+                    int numberOfPredicates = comparisonOutput[0].FinalComparisonObject.ToList().Count;
+                    int numberOfObjects = comparisonOutput.Count;
+                    //Second the table values
+
+                    ////////////////THE MAPS PART///////////
+                    if (canViewMap(comparisonOutput))
+                    {
+                        answerbox.InnerHtml += "<div class=\"comparisonbox\">" +
+                        "<div class=\"predicate\">" +
+                            //this returns the predicate of all the tables
+                        "Map" +
                         "</div>";
 
-            }
-            
-            answerbox.InnerHtml += "</div>" +
-                "</div>" +
-                "<div class=\"clearfix\">" +
-                "</div>" +
-            "</div>";
+                        answerbox.InnerHtml += "<div class=\"window\">" +
+                            "<div class=\"datarow\">";
+                        List<string> mapstrings = viewMap(comparisonOutput);
+                        foreach (string item in mapstrings)
+                        {
+                            answerbox.InnerHtml += "<div class=\"datacell expandableContent\">";
+                            answerbox.InnerHtml += item;
+                            answerbox.InnerHtml += "</div>";
 
-            //right till now
-            //will crash if no comparison........
-
-
-            //now for the data for each entity
-            int numberOfPredicates = comparisonOutput[0].FinalComparisonObject.ToList().Count;
-            int numberOfObjects = comparisonOutput.Count;
-            //Second the table values
-
-            ////////////////THE MAPS PART///////////
-            if (canViewMap(comparisonOutput))
-            {
-                answerbox.InnerHtml += "<div class=\"comparisonbox\">" +
-                "<div class=\"predicate\">" +
-                    //this returns the predicate of all the tables
-                "Map"+
-                "</div>";
-
-                answerbox.InnerHtml += "<div class=\"window\">" +
-                    "<div class=\"datarow\">";
-                List<string> mapstrings = viewMap(comparisonOutput);
-                foreach (string item in mapstrings)
-                {
-                    answerbox.InnerHtml += "<div class=\"datacell expandablecontent\">";
-                    answerbox.InnerHtml += item;
-                    answerbox.InnerHtml += "</div>";
-
-                }
-                answerbox.InnerHtml += "</div>" +
-                    "</div>" +
-                    "</div>";
-            }
-            ////////////////////////////////////////////////////////
+                        }
+                        answerbox.InnerHtml += "</div>" +
+                            "</div>" +
+                            "</div>";
+                    }
+                    ////////////////////////////////////////////////////////
 
 
-            for (int i = 0; i < numberOfPredicates; i++)
-            {
-                answerbox.InnerHtml += "<div class=\"comparisonbox\">" +
-                "<div class=\"predicate\">" +
-                    //this returns the predicate of all the tables
-                comparisonOutput[0].FinalComparisonObject[i].Key.Value +
-                "</div>";
+                    //comparisonOutput = removeUnwantedPredicates(comparisonOutput.ToList()).ToList();
 
-                answerbox.InnerHtml += "<div class=\"window\">" +
-                    "<div class=\"datarow\">";
-                
-
-                for (int j = 0; j < numberOfObjects; j++)
-                {
-                    answerbox.InnerHtml += "<div class=\"datacell expandablecontent\">";
-                    //should be in a href key and value
-                    for (int k = 0; k < comparisonOutput[j].FinalComparisonObject[i].Value.ToList().Count; k++)
+                    for (int i = 0; i < numberOfPredicates; i++)
                     {
-                        //we check if it's a uri or not
-                        string uri = comparisonOutput[j].FinalComparisonObject[i].Value[k].Key;
-                        string value = HttpUtility.HtmlDecode(comparisonOutput[j].FinalComparisonObject[i].Value[k].Value);
-                        if (Uri.IsWellFormedUriString(uri, UriKind.Absolute))
+                        //check if the predicate is of the ones that should be excluded 
+                        if (isExcludedURI(comparisonOutput[0].FinalComparisonObject[i].Key.Key))
+                            continue;
+
+
+
+                        answerbox.InnerHtml += "<div class=\"comparisonbox\">" +
+                        "<div class=\"predicate\">" +
+                            //this returns the predicate of all the tables
+                        comparisonOutput[0].FinalComparisonObject[i].Key.Value +
+                        "</div>";
+
+                        answerbox.InnerHtml += "<div class=\"window\">" +
+                            "<div class=\"datarow\">";
+
+
+                        for (int j = 0; j < numberOfObjects; j++)
                         {
-                            if (Uri.IsWellFormedUriString(value, UriKind.Absolute))
+                            answerbox.InnerHtml += "<div class=\"datacell expandableContent\">";
+                            //should be in a href key and value
+                            for (int k = 0; k < comparisonOutput[j].FinalComparisonObject[i].Value.ToList().Count; k++)
                             {
-                                answerbox.InnerHtml += "<a href=\"" + uri + "\">" + value + "</a><br/>";
+                                //we check if it's a uri or not
+                                string uri = comparisonOutput[j].FinalComparisonObject[i].Value[k].Key;
+                                string value = HttpUtility.HtmlDecode(comparisonOutput[j].FinalComparisonObject[i].Value[k].Value);
+                                if (Uri.IsWellFormedUriString(uri, UriKind.Absolute))
+                                {
+                                    if (Uri.IsWellFormedUriString(value, UriKind.Absolute))
+                                    {
+                                        answerbox.InnerHtml += "<span><a href=\"" + uri + "\">" + value + "</a><br/></span>";
+                                    }
+                                    else
+                                    {
+                                        answerbox.InnerHtml += "<span><a href=\"answer.aspx?uri=" + uri + "\">" + value + "</a><br/></span>";
+                                    }
+
+                                }
+
+                                else
+                                {
+                                    answerbox.InnerHtml += "<span>" + value + "<br/></span>";
+                                }
+
                             }
-                            else
-                            {
-                                answerbox.InnerHtml += "<a href=\"answer.aspx?uri=" + uri + "\">" + value + "</a><br/>";
-                            }
-
+                            answerbox.InnerHtml += "</div>";
                         }
+                        answerbox.InnerHtml += "</div>" +
+                            "</div>" +
+                            "</div>";
 
-                        else
-                        {
-                            answerbox.InnerHtml += value + "<br/>";
-                        }
+                    }
 
-                    }                    
                     answerbox.InnerHtml += "</div>";
+
                 }
-                answerbox.InnerHtml += "</div>" +
-                    "</div>" +
-                    "</div>";
+            }
+            catch (Exception c)
+            {
+
+                //logging should go here
+                Response.Redirect("error.aspx", false);
 
             }
-          
-            answerbox.InnerHtml += "</div>";
-
         }
 
 
@@ -222,16 +247,31 @@ namespace weetit_website
             foreach (ResourceInformation item in input)
             {
                 string temp = "<img src=\"http://maps.googleapis.com/maps/api/staticmap?center=";
-                temp += item.FinalComparisonObject.ToList().Find(e => e.Key.Key.Equals("http://www.w3.org/2003/01/geo/wgs84_pos#lat")).Value[0].Value.ToString();
+                string lat = item.FinalComparisonObject.ToList().Find(e => e.Key.Key.Equals("http://www.w3.org/2003/01/geo/wgs84_pos#lat")).Value[0].Value.ToString();
+                temp += lat;
                 //temp+=item.FinalComparisonObject.First(e=>e.Key.Key.
                 temp += ",";
-                temp += item.FinalComparisonObject.ToList().Find(e => e.Key.Key.Equals("http://www.w3.org/2003/01/geo/wgs84_pos#long")).Value[0].Value.ToString();
-                temp += "&zoom=10&size=400x400&sensor=false\">";
+                string longt = item.FinalComparisonObject.ToList().Find(e => e.Key.Key.Equals("http://www.w3.org/2003/01/geo/wgs84_pos#long")).Value[0].Value.ToString();
+                temp += longt;
+                temp += "&zoom=10&size=400x400&sensor=false\" title=\""+"longtitude="+longt+"\nlatitude="+lat+"\">";
                 toreturn.Add(temp);
             }
             return toreturn;
 
             
+ 
+        }
+
+
+
+        bool isExcludedURI(string input)
+        {
+            List<string> excludedURIs = new List<string>();
+            excludedURIs.Add("http://www.w3.org/2003/01/geo/wgs84_pos#long");
+            excludedURIs.Add("http://www.w3.org/2003/01/geo/wgs84_pos#lat");
+            excludedURIs.Add("http://www.w3.org/2003/01/geo/wgs84_pos#point");
+
+            return excludedURIs.Contains(input);
  
         }
     }
