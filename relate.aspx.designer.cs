3b3f709a0755fcb0b6030321fb45599b12b52cee
hadyelsahar <hadyelsahar@gmail.com>
2012-07-08 21:15:08 +0200

    overflow hidden to the comparison datacells

--- relate.aspx.designer.cs
+++ relate.aspx.designer.cs
@@ -31,6 +31,15 @@ namespace weetit_website {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// answerbox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl answerbox;
+        
+        /// <summary>
         /// headerTable control.
         /// </summary>
         /// <remarks>
